351a00bf272c4ee76cfbe8c2b0240bf6d28334da
ViqiRafianto <viqirafianto06@gmail.com>
2024-03-02 23:21:09 +0800

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TicTacToe from "./Components/Tictactoe/TicTacToe";
+
+function App() {
+  return (
+    <div>
+      <TicTacToe />
+    </div>
+  );
+}
+
+export default App;
